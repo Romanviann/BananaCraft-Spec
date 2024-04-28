9c566916a075bfcfe8084eb0cb8067072ec96399
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-28 15:17:54 -0400

    added data and implemented aircraft search

--- components/AircraftForm/AircraftForm.tsx
+++ components/AircraftForm/AircraftForm.tsx
@@ -1,19 +1,30 @@
 import "./AirCraftForm.css"
 import SpecModal from "../SpecModal/SpecModal.tsx";
 import {useState} from "react";
+import aircraft from "../../data/aircraft.json";
+import ISpecProps from "../SpecModal/ISpecProps.ts"
+
 
 export default function AircraftForm() {
-    const [resultFound, setResultFound] = useState(false);
+    const [aircraftSpecs, setAircraftSpecs] = useState<ISpecProps>({});
+    const [aircraftFound, setAircraftFound] = useState<boolean>(false);
 
-    function toggle() {
-        setResultFound(prevState => {
-            return !prevState;
-        });
+    function findAircraft(e: { preventDefault: () => void; target: any; }) {
+        e.preventDefault();
+        const formData = Object.fromEntries((new FormData(e.target)).entries());
+
+        const regex = new RegExp(`${formData.manufacturer} ${formData.model}`, 'i');
+        const filteredAircraft = aircraft.filter((aircraft) => regex.test(aircraft.Model_FAA));
+
+        filteredAircraft.sort((a, b) => a.Model_FAA.length - b.Model_FAA.length);
+        setAircraftSpecs(() => filteredAircraft[0]);
+        setAircraftFound(() => true);
+        return filteredAircraft[0];
     }
 
     return (
         <>
-            <form>
+            <form onSubmit={findAircraft}>
                 <div className={"row form-container"}>
                     <div className={"col-12 mb-4 col-sm-6 col-md-4 col-lg-3"}>
                         <input required={true} name={"manufacturer"} aria-label={"Manufacturer"} maxLength={30}
@@ -28,13 +39,15 @@ export default function AircraftForm() {
 
                     <div className={"row"}>
                         <div className="d-grid gap-2 col-6 mx-auto">
-                            <button className="btn btn-lg btn-warning" type="button" onClick={()=>toggle()}>Get Dimensions</button>
-                            <button className="button-link" type="button">Feeling Lucky</button>
+                            <button className="btn btn-lg btn-warning" type="submit">Get Dimensions</button>
+                            <button className="button-link" type="submit">Feeling Lucky</button>
                         </div>
                     </div>
                 </div>
             </form>
-            {resultFound && <SpecModal show={resultFound}  onHide={() => toggle()} /> }
+
+            {aircraftFound &&
+                <SpecModal aircraftSpecs={aircraftSpecs} show={aircraftFound} onHide={() => setAircraftFound(() => false)} /> }
 
         </>
     );
