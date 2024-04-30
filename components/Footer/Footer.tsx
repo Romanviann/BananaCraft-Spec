10a5072fb07db4ac1a352e7b5362c974d05c09a0
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-29 23:22:35 -0400

    fixed broken Github logo issue #4

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function footer() {
         <nav className="footer navbar fixed-bottom ">
             <div className="container-fluid">
                 <span>BananaCraft Spec. 2024</span>
-                <a href={"https://github.com/Romanviann/BananaCraft-Spec"} target={"_blank"}><img src={"/src/assets/images/icons8-github-30.svg"}/></a>
+                <a href={"https://github.com/Romanviann/BananaCraft-Spec"} target={"_blank"}><img src={"./src/assets/images/icons8-github-30.svg"}/></a>
                 <a className="link navbar-brand" target={"_blank"} href="https://medium.com/@liamfoley2430/on-the-usefulness-of-bananas-a-new-system-of-measurement-76c50d8bb89a">🍌 Banana Standard (BS)</a>
             </div>
         </nav>
