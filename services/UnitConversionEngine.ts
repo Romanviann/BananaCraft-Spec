cec28e607cf2e90674bdb99b547a0bf48fda74ef
Omar Jawhar <omarjawhar.10@gmail.com>
2024-04-29 22:07:51 -0400

    added method to convert strings containing commas and decimal points

--- services/UnitConversionEngine.ts
+++ services/UnitConversionEngine.ts
@@ -1,5 +1,22 @@
 class UnitConversionEngine {
 
+    public static convertFeetToBanana(lengthInFeet: number | string): string | null {
+        return lengthInFeet ? Math.round((this.stringToFloat(lengthInFeet) / 0.656988189) * 100) / 100 + ' bananas' : null;
+    }
+
+    public static convertKnToKbh(speedInKnots: number | string): string | null {
+        const speedInKilometerPerHour = this.stringToFloat(speedInKnots) * 1.852;
+        return speedInKilometerPerHour ? Math.round(speedInKilometerPerHour * (Math.pow(10, 6)) * (1 / 200.25) * (1 / 1000) * 100) / 100 + ' kilobananas/h' : null;
+    }
+
+    public static convertPoundToBananagram(massInPounds: number | string): string | null {
+        return massInPounds ? Math.round((this.stringToFloat(massInPounds) / 0.398) * 100) / 100 + ' bananagrams' : null;
+    }
+
+    public static convertFtSquaredToSquareBanana(areaInFeetSquared: number | string): string | null {
+        return areaInFeetSquared ? Math.round((this.stringToFloat(areaInFeetSquared) / 0.431633) * 100) / 100 + " square bananas" : null;
+    }
+
     private static stringToFloat(input: any) {
 
         if (typeof (input) == "string") {
@@ -8,24 +25,6 @@ class UnitConversionEngine {
             return input;
         }
     }
-
-    public static convertFeetToBanana(lengthInFeet: number | string): number {
-        return Math.round((this.stringToFloat(lengthInFeet) / 0.656988189) * 100) / 100;
-    }
-
-    public static convertKnToKbh(speedInKnots: number | string): number {
-        const speedInKilometerPerHour = this.stringToFloat(speedInKnots) * 1.852;
-        return Math.round(speedInKilometerPerHour * (Math.pow(10, 6)) * (1 / 200.25) * (1 / 1000) * 100) / 100;
-    }
-
-    public static convertPoundToBananagram(massInPounds: number | string): number {
-        return Math.round((this.stringToFloat(massInPounds) / 0.398) * 100) / 100;
-    }
-
-    public static convertFtSquaredToSquareBanana(areaInFeetSquared: number | string): number {
-        return Math.round((this.stringToFloat(areaInFeetSquared) / 0.431633) * 100) / 100;
-    }
-
 }
 
 export default UnitConversionEngine;
